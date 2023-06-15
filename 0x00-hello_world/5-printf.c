aaada5de330d24bd21cd39baf64bd4c24e757a45
BBouhanou <b.bouhanou@outlook.com>
2023-06-15 11:10:11 -0700

    ok

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,18 +1,12 @@
 #include <stdio.h>
 /**
-*main - Main function
-*
-*
-*return: return 0 everytime.
-*/
+ * main - Print a string
+ * Description: use printf
+ * Return: 0
+ */
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
-<<<<<<< HEAD
-
-=======
-
-
->>>>>>> b96480a484f62a7e8816f7de01581e16eba4f454
