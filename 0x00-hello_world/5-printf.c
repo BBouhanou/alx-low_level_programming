4c9ed01ee1eb3e14bc94f706a1521a9883ab6065
BBouhanou <132355564+BBouhanou@users.noreply.github.com>
2023-06-15 18:12:33 +0100

    s

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -10,4 +10,9 @@ int main(void)
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
+<<<<<<< HEAD
 
+=======
+
+
+>>>>>>> b96480a484f62a7e8816f7de01581e16eba4f454
