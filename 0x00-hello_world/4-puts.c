e2e4f04ec8776a95fcfe8d5fd63567defe9d9703
BBouhanou <132355564+BBouhanou@users.noreply.github.com>
2023-06-15 16:45:11 +0100

    d

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
+#include <string.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+*main - Main function of my project
+*
+*
+*Return: return 0 everytime
+*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+
+	char displayed[52];
+
+	strcpy(displayed, "\"Programming is like building a multilingual puzzle");
+	puts(displayed);
 	return (0);
 }
->>>>>>> cd07e75a0f8b00a320c85ff0c311a864971f4162
